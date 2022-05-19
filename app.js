2067455ba779195a35406f0f47a3976a5d333115
Liidia Laada <liidia.laada@gmail.com>
2022-05-19 12:27:20 +0300

    GET

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+app.get('/hello', function(request, response) {
+    response.send('Hello world!');
+})
+
 app.listen(PORT, function() {
     console.log('Server is running on PORT 5000');
 })
